8f9e308908682355188e469d2f9941c27e872d51
PrimakMV <pm2710@mail.ru>
2025-02-11 00:06:45 +0300

    Added new task solutions

---
+++ BinarySearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,36 @@
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int target = 1;
+        int[] nums = new int[]{3,4,5,6,1,2};
+
+        SearchInRotatedSortedArray s = new SearchInRotatedSortedArray();
+
+        System.out.println(s.search(nums, target));
+    }
+
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int m = l + ((r - l) / 2);
+            if (nums[m] == target) return m;
+
+            if (nums[l] <= nums[m]) {
+                if (target >= nums[l] && nums[m] > target) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (target > nums[m] && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
